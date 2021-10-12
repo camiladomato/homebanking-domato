edd2d77e38f983d28030404d7dbcf6465b806d2c
camiladomato <camiladomato@gmail.com>
2021-10-12 10:29:19 -0300

    primer commit

---
+++ homebanking/src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -0,0 +1,36 @@
+package com.mindhub.homebanking;
+
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HomebankingApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository) {
+		return (args) -> {
+			Client client1 = new Client();
+			client1.setEmail("melba@mindhub.com");
+			client1.setFirstName("Melba");
+			client1.setLastName("Morel");
+
+			Client client2 = new Client();
+			client2.setEmail("ddddd");
+			client2.setFirstName("ffff");
+			client2.setLastName("555");
+
+			clientRepository.save(client1);
+			clientRepository.save(client2);
+		};
+	}
+
+}
---
+++ homebanking/src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,63 @@
+package com.mindhub.homebanking.models;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long  id;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    public Client() {
+    }
+    public Client(String email, String firstName, String lastName){
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
